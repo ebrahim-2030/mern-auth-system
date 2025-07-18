c481d036ea6696966f056236a7e2b8d565f8aef5
ebrahim <ebrahimasil@gmail.com>
2025-07-18 04:48:54 +0430

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+// define the user schema
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 20 ,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    profilePicture: {
+        type: String,
+        default: "",
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+}, {timestamps: true});
+
+// create the user moldel using the schema
+const User = mongoose.model('User', userSchema);
+// export the user model
+export default User;
